ec3b450f610c4171fc64a96188f0a36a082f6a2c
Max Prudhomme <account@maxprudhomme.com>
2026-04-23 00:14:41 +0200

    Initial Commit

---
+++ crystallized/crystallizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  crystallizedApp.swift
+//  crystallized
+//
+//  Created by Max PRUDHOMME on 23/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct crystallizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
